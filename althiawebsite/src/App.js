6a0d7882b8fd797b6a75eb63be4797ec45024e45
monoguitari <jesus_rodriguez@brown.edu>
2024-05-14 17:41:55 -0400

    mission page

--- althiawebsite/src/App.js
+++ althiawebsite/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { MantineProvider } from '@mantine/core';
+import  Mission  from './components/Mission';
+
+export default function App() {
+  return <MantineProvider><Mission/></MantineProvider>;
 }
-
-export default App;
