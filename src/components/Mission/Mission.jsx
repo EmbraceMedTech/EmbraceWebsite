ca33539cffda6db79218bb16ad40e90039daa90a
Kenneth Liou <kennethliou01@gmail.com>
2024-05-17 20:20:48 -0400

    commented out old import statement

--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -1,5 +1,5 @@
 import { Image, Container, Title, Button, Group, Text, List, ThemeIcon, rem } from '@mantine/core';
-import { IconCheck } from '@tabler/icons-react';
+// import { IconCheck } from '@tabler/icons-react';
 // import image from './image.svg';
 import classes from './Mission.module.css';
 
