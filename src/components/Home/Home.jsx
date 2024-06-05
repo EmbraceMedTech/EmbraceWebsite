1ecf4e2db1141fac4d52752149f512e7560a9ed3
Kenneth Liou <90430079+kungfu-k3nny@users.noreply.github.com>
2024-06-05 14:41:26 -0700

    background video

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,17 @@
 import { Image, Container, Title, Button, Group, Text, List, ThemeIcon, rem } from '@mantine/core';
 // import classes from './Home.module.css';
+import React from 'react'
+import backgroundVideo from '../../assets/background_video.mp4'
 
 export default function Home() {
   return (
-    <div id="home" className="section">
-      <h1 className='h1-intro'>Althia</h1>
-      <p>Intro blurb</p>
+    <div className="home">
+      <div className='overlay'></div>
+      <video className="background_video" src={backgroundVideo} autoPlay loop muted />
+      <div className='content'>
+        <h1 className='h1-intro'>Althia</h1>
+        <p>Intro blurb</p>
+      </div>
     </div>
 );
 }
