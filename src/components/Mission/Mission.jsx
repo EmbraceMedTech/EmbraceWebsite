d93f5b655eadf58e867ef42355dc9971497602a9
monoguitari <jesus_rodriguez@brown.edu>
2024-07-30 20:37:48 -0400

    fix: mission text
    Text in mission section used to always be horizontally spaced, now it goes under image if screen is small enough (tablet/phone)

--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -17,7 +17,7 @@ export default function Mission() {
             Over 600,000 amputees live in pain or discomfort due to an uncomfortable socket. A majority of these amputees simply want a limb that they can comfortably walk in during the day, but the rigidity of current sockets do not allow for an expansion or contraction of the limb, which is often the culprit of discomfort. While some mechanically adjusting sockets do exist, they are tedious to use, and very hard to implement due to complicated methods of tightening and loosening. 
           </Text>
           <Text c="dimmed" mt="md">
-           With our automatically adjusting prosthetic socket, we will be able to let amputees focus on doing what they love, and give them their mobility back. 
+            With our automatically adjusting prosthetic socket, we will be able to let amputees focus on doing what they love, and give them their mobility back. 
           </Text>
         </div>
       </div>
