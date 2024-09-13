cfbcca70d4e39c83766e50d3dbffa0849c22057a
Kenneth Liou <kennethliou01@gmail.com>
2024-09-12 20:58:31 -0700

    added a new webhook url

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
 const app = express();
 const port = 5000;
+
+// Access the API_URL from environment variables
+const API_URL = process.env.CONTACT_WEBHOOK_URL;
 
 app.use(bodyParser.json());
 
@@ -10,13 +18,14 @@ app.post('/api/send-to-slack', async (req, res) => {
   const { text } = req.body;
 
   try {
-    await fetch('https://hooks.slack.com/services/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', {
+    await fetch(API_URL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ text })
     });
+    res.status(200).send('Message sent to Slack!');
   } catch (error) {
     res.status(500).send('Something went wrong submitting the form! You can email us instead (found at the bottom of the page).');
   }
